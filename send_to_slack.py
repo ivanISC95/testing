95f6c18200385531529dbc19a18dd10970aae9c6
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2025-05-30 13:23:00 -0600

    Update BOT Slack messages
    Add branchs to push in the messages to slack

--- send_to_slack.py
+++ send_to_slack.py
@@ -6,10 +6,11 @@ channel_id = os.environ['SLACK_CHANNEL_ID']
 
 from_name = os.environ.get("GITHUB_ACTOR", "desconocido")
 commit_msg = os.environ.get("COMMIT_MESSAGE", "Sin mensaje")
+rama = os.environ.get("GITHUB_RAMA", "Sin RAMA")
 
 client = WebClient(token=slack_token)
 
 client.chat_postMessage(
     channel=channel_id,
-    text=f"🚀 Nuevo push por *{from_name}*\n📝 Commit: {commit_msg}"
+    text=f"👤 Rama : *{rama}  🚀 Nuevo push por *{from_name}*\n📝 Commit: {commit_msg}"
 )
